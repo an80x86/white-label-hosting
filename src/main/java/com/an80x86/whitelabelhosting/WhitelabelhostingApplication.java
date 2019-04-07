71b96f30019b1869214b4fd363803000e554206c
Mustafa Bayer <an80x86@gmail.com>
2019-04-08 01:41:27 +0300

    spring batch eklendi.

--- src/main/java/com/an80x86/whitelabelhosting/WhitelabelhostingApplication.java
+++ src/main/java/com/an80x86/whitelabelhosting/WhitelabelhostingApplication.java
@@ -2,12 +2,13 @@ package com.an80x86.whitelabelhosting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WhitelabelhostingApplication {
-
     public static void main(String[] args) {
+
         SpringApplication.run(WhitelabelhostingApplication.class, args);
     }
-
 }
