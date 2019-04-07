6582e9d12d5ed73a10ebc012c39279c9c9592b08
Mustafa Bayer <an80x86@gmail.com>
2019-04-07 18:38:32 +0300

    first commit

---
+++ src/main/java/com/an80x86/whitelabelhosting/WhitelabelhostingApplication.java
@@ -0,0 +1,13 @@
+package com.an80x86.whitelabelhosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhitelabelhostingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhitelabelhostingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/an80x86/whitelabelhosting/WhitelabelhostingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.an80x86.whitelabelhosting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhitelabelhostingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
